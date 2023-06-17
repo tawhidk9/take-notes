486c96946be7d79cac4897e7df9163aef58534f4
Tawhid <tawhidkamal5@gmail.com>
2023-06-16 23:59:39 -0400

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require ("path");
 const { clog } = require('./middleware/clog.js');
 const api = require("./routers/index.js");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT||3001;
 
 const app = express();
 
